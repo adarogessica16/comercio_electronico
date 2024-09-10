6fc7ffc84335456dcccce82c1f3a75fb7a757ec9
Gessica <adarogessica@gmail.com>
2024-09-09 22:05:22 -0400

    dtos de categorias

--- BeansDto/src/main/java/com/beansdto/dtos/categoria/CategoriaDTO.java
+++ BeansDto/src/main/java/com/beansdto/dtos/categoria/CategoriaDTO.java
@@ -1,4 +1,22 @@
 package com.beansdto.dtos.categoria;
 
-public class CategoriaDTO {
+import com.beansdto.dtos.base.BaseDTO;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import lombok.Getter;
+import lombok.Setter;
+
+@XmlRootElement(name = "categoria")
+@Getter
+@Setter
+public class CategoriaDTO extends BaseDTO {
+
+    private static final long serialVersionUID = 1L;
+    private String nombre;
+    private String descripcion;
+
+    @Override
+    public String toString() {
+        return "CategoriaDTO [nombre=" + nombre + ", descripcion=" + descripcion+ "]";
+    }
 }
--- BeansDto/src/main/java/com/beansdto/dtos/categoria/CategoriaResult.java
+++ BeansDto/src/main/java/com/beansdto/dtos/categoria/CategoriaResult.java
@@ -1,4 +1,16 @@
 package com.beansdto.dtos.categoria;
 
-public class CategoriaResult {
+import com.beansdto.dtos.base.BaseResult;
+import java.util.List;
+import javax.xml.bind.annotation.XmlElement;
+
+public class CategoriaResult extends BaseResult<CategoriaDTO> {
+    private static final long serialVersionUID = 1L;
+    @XmlElement
+    public List<CategoriaDTO> getCategorias() {
+        return getList();
+    }
+    public void setCategorias(List<CategoriaDTO> dtos) {
+        super.setList(dtos);
+    }
 }
