8442e2ede1f4fb98170f2f889d513699bc173ce5
Gessica <adarogessica@gmail.com>
2024-09-09 22:05:38 -0400

    dtos de inventarios

--- BeansDto/src/main/java/com/beansdto/dtos/inventario/InventarioResult.java
+++ BeansDto/src/main/java/com/beansdto/dtos/inventario/InventarioResult.java
@@ -1,4 +1,16 @@
 package com.beansdto.dtos.inventario;
 
-public class InventarioResult {
+import com.beansdto.dtos.base.BaseResult;
+import javax.xml.bind.annotation.XmlElement;
+import java.util.List;
+
+public class InventarioResult extends BaseResult<InverntarioDTO> {
+    private static final long serialVersionUID = 1L;
+    @XmlElement
+    public List<InverntarioDTO> getInventarios() {
+        return getList();
+    }
+    public void setInventarios(List<InverntarioDTO> dtos) {
+        super.setList(dtos);
+    }
 }
--- BeansDto/src/main/java/com/beansdto/dtos/inventario/InverntarioDTO.java
+++ BeansDto/src/main/java/com/beansdto/dtos/inventario/InverntarioDTO.java
@@ -1,4 +1,24 @@
 package com.beansdto.dtos.inventario;
 
-public class InverntarioDTO {
+
+import com.beansdto.dtos.base.BaseDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "inventario")
+@Getter
+@Setter
+public class InverntarioDTO extends BaseDTO {
+    private static final long serialVersionUID = 1L;
+    private Integer Id_Producto;
+    private Integer Id_Usuario;
+    private int cantidad;
+
+    @Override
+    public String toString() {
+        return "InventarioDTO [ idProducto=" + Id_Producto + ", idUsuario=" + Id_Usuario+ "Cantidad" +cantidad+ "]";
+    }
+
 }
