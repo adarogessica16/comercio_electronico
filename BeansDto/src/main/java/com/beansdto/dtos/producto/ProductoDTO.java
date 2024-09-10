8d63d7756277f21bb8428de1a08deb503ca58f7c
Gessica <adarogessica@gmail.com>
2024-09-09 22:05:50 -0400

    dtos de productos

--- BeansDto/src/main/java/com/beansdto/dtos/producto/ProductoDTO.java
+++ BeansDto/src/main/java/com/beansdto/dtos/producto/ProductoDTO.java
@@ -1,4 +1,25 @@
 package com.beansdto.dtos.producto;
 
-public class ProductoDTO {
+import com.beansdto.dtos.base.BaseDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "producto")
+@Getter
+@Setter
+public class ProductoDTO extends BaseDTO {
+    private static final long serialVersionUID = 1L;
+    private Integer Id_Categoria;
+    private Integer Id_Usuario;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+
+    @Override
+    public String toString() {
+        return "ProductoDTO [idCategoria=" + Id_Categoria + ", idUsuario=" + Id_Usuario+ "nombre=" + nombre + ", descripcion=" + descripcion + ", precio=" + precio + "]";
+    }
 }
+
--- BeansDto/src/main/java/com/beansdto/dtos/producto/ProductoResult.java
+++ BeansDto/src/main/java/com/beansdto/dtos/producto/ProductoResult.java
@@ -1,4 +1,16 @@
 package com.beansdto.dtos.producto;
 
-public class ProductoResult {
+import com.beansdto.dtos.base.BaseResult;
+import javax.xml.bind.annotation.XmlElement;
+import java.util.List;
+
+public class ProductoResult extends BaseResult<ProductoDTO> {
+    private static final long serialVersionUID = 1L;
+    @XmlElement
+    public List<ProductoDTO> getProductos() {
+        return getList();
+    }
+    public void setProductos(List<ProductoDTO> dtos) {
+        super.setList(dtos);
+    }
 }
